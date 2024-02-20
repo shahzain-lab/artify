c3a6365e0d69b7b506d00966b0530b9cb6a2cd35
shahzain-lab <shahzain30196@gmail.com>
2024-02-20 15:21:43 +0500

    fixed state

--- src/components/dashboard/layout/sidebar/index.tsx
+++ src/components/dashboard/layout/sidebar/index.tsx
@@ -30,11 +30,13 @@ const Sidebar = () => {
   return (
     <>
     <div className={`${openMobileMenu ? 'flex w-[80%] z-30 fixed h-screen overflow-y-scroll pb-4 no-scrollbar' : 'hidden'} md:w-[20%] md:fixed md:h-[96%] md:flex flex-col justify-between px-3 py-2 bg-dark-800 rounded-[12px]`}>
-    <div className='w-full h-screen fixed top-0 left-0 bg-bg-layer backdrop-blur-sm'>
-      <p onClick={handleClose} className='absolute top-4 right-4 border-2 border-gray-300 p-1 rounded-[8px]'>
-       <Cross />
-      </p>
-    </div>
+    {openMobileMenu && (
+      <div className='w-full h-screen fixed top-0 left-0 bg-bg-layer backdrop-blur-sm'>
+        <p onClick={handleClose} className='absolute top-4 right-4 border-2 border-gray-300 p-1 rounded-[8px]'>
+        <Cross />
+        </p>
+      </div>
+    )}
         <div className='z-20 md:z-auto'> 
             <Profile />
             <Divider />
