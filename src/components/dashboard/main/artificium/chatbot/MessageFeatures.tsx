1dc234a607baca71ff81081c0a41bd97a6c6ffdc
shahzain-lab <shahzain30196@gmail.com>
2024-02-20 15:43:44 +0500

    added ts types

--- src/components/dashboard/main/artificium/chatbot/MessageFeatures.tsx
+++ src/components/dashboard/main/artificium/chatbot/MessageFeatures.tsx
@@ -10,16 +10,10 @@ interface Props {
 const MessageFeatures = ({features}: Props) => {
   return (
     <div className='flex items-center gap-3'>
-        {features.map((feat) => (
-            <div key={feat.src} className='flex flex-col items-center p-6 gap-2 mt-5 bg-glass-fill rounded-[12px]'>
+        {features.map((feat, i) => (
+            <div key={i} className='flex flex-col items-center p-6 gap-2 mt-5 bg-glass-fill rounded-[12px]'>
              <span className='bg-glass-fill py-[13px] px-1 rounded-[100%]'>
-                 <Image
-                     src={feat.src}
-                     alt={feat.title}
-                     width={45}
-                     height={45}
-                     className='-mb-5'
-                 />
+                 {feat.Icon}
              </span>
              <Typography variant='semibold' size='ms' className=''>{feat.title}</Typography>
             </div>
